d141e3caa5b2e9b0fc0741d54eee2f238faabfd4
Mila Latifah Zahra <152600717+MilaLatifah@users.noreply.github.com>
2023-12-01 16:35:55 +0700

    mila cantik

---
+++ belajar.py
@@ -0,0 +1,9 @@
+daftar_nama = ["andika","amelani","verlyza","salman","zebian"]
+daftar_nilai_pplg1 = [80,90,100,95,90,80]
+
+jumlah = len(daftar_nama)
+total = sum(daftar_nilai_pplg1)
+
+print(daftar_nama[3])
+print("jumlah pplg 1 : ", jumlah)
+print("total nilai ", total)
